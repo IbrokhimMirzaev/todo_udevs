3f97b823fb95f25c956c716ed928b00f3503652b
IbrokhimMirzaev <mirzaevibrokhim@gmail.com>
2023-03-27 21:47:41 +0500

    new changes

--- lib/main.dart
+++ lib/main.dart
@@ -36,6 +36,7 @@ class MyApp extends StatelessWidget {
           debugShowCheckedModeBanner: false,
           onGenerateRoute: AppRouter.onGenerateRoute,
           initialRoute: (LocalData.getBool(key: "isOnBoarding") == true) ? "/" : "/on_boarding",
+
         );
       },
     );
